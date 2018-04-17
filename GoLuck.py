22f10481713ebc129cac88349701e31c8c0ba793
saurabh kumar giri <skgvns8789@gmail.com>
2018-04-17 15:32:59 +0530

    Update GoLuck.py

--- GoLuck.py
+++ GoLuck.py
@@ -1,11 +1,19 @@
 import random as r
 s = r.randint(0,100)
 print("Enter Your guesses(2 Players)")
-player1 = int(input("Player 1 : "))
-player2 = int(input("Player 2 : "))
+p1 = input("Name Player1:")
+p2= input("Name Player2:")
+player1 = int(input(p1+" : "))
+player2 = int(input(p2+" : "))
 a = {}
-a["p1"] = (abs(player1 - s))
-a["p2"] = (abs(player2 - s))
+a[p1] = (abs(player1 - s))
+a[p2] = (abs(player2 - s))
 
 print(a,s)
+
+l = min(a.values())
+for i in a.keys():
+    if a[i] == l:
+        print("Winner is ",i,"with value",l)
+
 
